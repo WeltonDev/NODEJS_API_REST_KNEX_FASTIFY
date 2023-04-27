6e80ea0ec6c8f347266cd5065ed38dbc82f49917
WeltonDev <welton.developer95@gmail.com>
2023-04-27 19:29:13 -0300

    production config

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,9 @@ import { knex as knexSetup, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DB_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite' ? { filename: env.DB_URL } : env.DB_URL,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   DB_URL: z.string(),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   PORT: z.coerce.number().default(3333),
 })
 
